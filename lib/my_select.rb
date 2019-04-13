19d6994e6eecc1584aace9467737bab3ed14d31e
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-13 16:42:26 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,7 @@
 def my_select(collection)
   empty_array = []
   saved_block = Proc.new {
-    my_select(empty_array) do |x|
-      raise "This block should not run!"
+    my_select(empty_array) do |x
     end
 
-end
+
